bf3c8b836eb343617760108290e87cb05c675a04
jeanvalentin51 <valentin.j.gheorghe.com>
2019-10-21 09:31:04 -0400

    added logic for half

--- MathUtilities.java
+++ MathUtilities.java
@@ -28,7 +28,7 @@ public class MathUtilities{
    * @return the half of the number in double
    */
   public Double half(Integer number) {
-      return null;
+      return number/Double.valueOf(2);
   }
 
   /**

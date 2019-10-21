86f9083d87cfad09a8383c792f04fac718bb88d6
jeanvalentin51 <valentin.j.gheorghe.com>
2019-10-21 09:26:33 -0400

    add MathUtils.

--- MathUtilities.java
+++ MathUtilities.java
@@ -9,7 +9,7 @@ public class MathUtilities{
    * @return the sum of the two numbers
    */
   public Integer add(Integer baseValue, Integer valueToAdd){
-      return null;
+      return baseValue + valueToAdd;
   }
 
   /**
@@ -19,7 +19,7 @@ public class MathUtilities{
    * @return the sum of the two numbers
    */
   public Double add(Double baseValue, Double valueToAdd){
-      return null;
+      return baseValue + valueToAdd;
   }
 
   /**
